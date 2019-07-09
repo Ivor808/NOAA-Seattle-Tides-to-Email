23b3fe4c5ac20cd5eb35a4f75c41bfadb625c6a2
Ivor808 <ivor808@gmail.com>
2019-07-08 18:34:52 -0700

    Reformat using black
    reformatted using black
    
    https://github.com/python/black

--- test.py
+++ test.py
@@ -4,11 +4,9 @@ import tide_scraper
 
 
 class MyTestCase(unittest.TestCase):
-
-
     def test_link_to_csv_to_list(self):
         isinstance(tide_scraper.link_to_csv(tide_scraper.seattle_tide_csv_link), list)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
